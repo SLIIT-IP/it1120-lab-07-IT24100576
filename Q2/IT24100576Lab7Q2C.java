89330f2c04a7cebef26b1ef1e70ed8b5eb7c9e45
IT24100576 <it24100576@my.sliit.lk>
2024-10-04 01:21:02 +0530

    Add  IT24100576Lab7Q2C to Q2C

---
+++ Q2/IT24100576Lab7Q2C.java
@@ -0,0 +1,15 @@
+public class IT24100576Lab7Q2C{
+
+    public static void main(String[] args) {
+        
+        for (int number = 5; number >= 1; number--) {
+            
+            for (int count = 1; count <= number; count++) {
+                System.out.print(number);
+            }
+
+            
+            System.out.println();
+        }
+    }
+}
